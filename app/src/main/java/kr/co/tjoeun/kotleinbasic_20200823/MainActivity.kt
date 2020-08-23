9a45c535835bb1cb5bc1a743cf9630309872151b
박성현 <park2light@gmail.com>
2020-08-23 11:34:18 +0900

    로그남기기 버튼 별도 추가  - 각각의 버튼에 따라 이벤트 처리 가능

--- app/src/main/java/kr/co/tjoeun/kotleinbasic_20200823/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotleinbasic_20200823/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +15,13 @@ class MainActivity : AppCompatActivity() {
 //            클릭미 버튼이 눌리면 실행될 코드를 적는  {}
             Log.d("메인화면","클릭미버튼이 눌림")
         }
+
+//        XML에 추가한 버튼을 makeLogBtn으로 이름지어서 => 눌리면 로그남기기 버튼 눌림 로그 남겨보기
+        makeLogBtn.setOnClickListener {
+
+            Log.d("메인화면","로그남기기 버튼눌림")
+
+        }
+
     }
 }
