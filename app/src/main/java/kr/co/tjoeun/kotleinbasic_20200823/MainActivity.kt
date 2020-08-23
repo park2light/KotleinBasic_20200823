d20b8eed5d21108f63bb4a4f27238b50e2bd75af
박성현 <park2light@gmail.com>
2020-08-23 11:11:46 +0900

    버튼이 눌리면 => 로그를 남긴다

--- app/src/main/java/kr/co/tjoeun/kotleinbasic_20200823/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotleinbasic_20200823/MainActivity.kt
@@ -2,10 +2,17 @@ package kr.co.tjoeun.kotleinbasic_20200823
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickMeBtn.setOnClickListener {
+//            클릭미 버튼이 눌리면 실행될 코드를 적는  {}
+            Log.d("메인화면","클릭미버튼이 눌림")
+        }
     }
 }
